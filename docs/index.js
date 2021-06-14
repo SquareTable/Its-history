bd6ad8a764333e6789059029bc384b206d32fe1e
Sebastian Webster <84299475+Sebastian-Webster@users.noreply.github.com>
2021-06-14 12:18:09 +1200

    add a button

--- docs/index.js
+++ docs/index.js
@@ -1,1 +1,17 @@
 /* This is here just in case we need to add JS to the extension page */
+function show_video_demonstration() {
+    var x = document.getElementById('video_demonstration');
+    if(x.className == "invisible") {
+        x.className = "visible";
+        var x = document.getElementById('video_demonstration_showhide_button');
+        x.textContent = "Hide video demonstration";
+    }
+    else {
+        x.className = "invisible";
+        var x = document.getElementById('video_demonstration_showhide_button');
+        x.textContent = "Show video demonstration";
+    }
+}
+window.onload = function() {
+    /*onload code goes here */
+}
